9a96ecd661bf36385b15829bcb14a3c8ea1faef8
Navrozbek <github.navrozbek.352@gmail.com>
2025-10-02 16:31:28 +0500

    Bu 30 daqiqalik masalalar !

---
+++ main.py
@@ -0,0 +1,72 @@
+""" Pythonda arifmetik operatorlar mavzusidan algoritmik masalalar  """
+
+# 1.1
+
+# a = int(input("A-sonni kiriting: "))
+# b = int(input("B-sonni kiriting: "))
+# print("Javob:",a+b)
+
+# 1.2
+
+# L = int(input("Uzunligini kiriting: "))
+# W = int(input("Enini kiriting: "))
+# print("Yuzasi: ",L*W)
+
+# 1.3
+
+# x = int(input("x-sonni kiriting: "))
+# y = int(input("y-sonni kiriting: "))
+# z = int(input("z-sonni kiriting: "))
+# print(f"Uch ta sonni o'rta airfmetigi: {x+y+z // 3}")
+
+# 1.4
+
+# a = int(input("A-sonni kiriting: "))
+# b = int(input("B-sonni kiriting: "))
+# print("Peremetri:",(2 * (a+b)))
+
+# 1.5
+
+# h = int(input("Soatni kiriting: "))
+# m = int(input("Minutni kiriting: "))
+#
+# print(f"Javob: {(h*60)+m} minut!")
+
+# 1.6
+
+# usd = int(input("$ ni kiriting: "))
+# print(f"Sizda: {usd*11_500} Ming So'm bor !")
+
+# 1.7
+
+# a = int(input("A-sonni kiriting: "))
+# b = int(input("B-sonni kiriting: "))
+# farq = abs(a - b)
+# print("Farqi: ",farq)
+
+# 1.8
+
+# x = int(input("x-sonni kiriting: "))
+# n = int(input("n-sonni kiriting: "))
+# daraja = x**n
+# print("X ni n darajasi: ",daraja)
+
+# 1.9
+
+# x = int(input("x-sonni kiriting: "))
+# n = int(input("n-sonni kiriting: "))
+# son = x//n
+# q = x%n
+# print("Sonni ozi: ",son, "Qoldiqi: ", q)
+
+# 2
+
+# a = int(input("A-sonni kiriting: "))
+# b = int(input("B-sonni kiriting: "))
+#
+# a,b = b,a
+#
+# print(f"Javob: \n A: {a} \n B: {b}")
+
+# 2.1
+
